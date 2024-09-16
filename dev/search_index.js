3ad267e26a768d57191dce2f590a76760b4ebf28
Documenter.jl <documenter@juliadocs.github.io>
2024-09-16 20:13:21 +0000

    build based on d4f6eac

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EndogenousOligopsony","category":"page"},{"location":"#EndogenousOligopsony","page":"Home","title":"EndogenousOligopsony","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EndogenousOligopsony.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EndogenousOligopsony]","category":"page"}]
+}
